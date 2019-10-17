de157dc3b179a0acdd45be68e3be8d679a48c40a
wudeng <wudeng.wd@alibaba-inc.com>
2019-10-17 18:04:25 +0800

    add poormanprofile.sh

---
+++ poormanprofile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]]; then
+    echo "$0 PID OUTPU_FILE"
+    exit
+fi
+
+nsamples=100
+sleeptime=0
+#pid=$(pidof lua)
+pid=$1
+output=$2
+
+
+for x in $(seq 1 $nsamples)
+do
+    gdb -ex "set pagination 0" -ex "thread apply all bt" -batch -p $pid
+    sleep $sleeptime
+done | awk 'BEGIN { s = ""; }
+/^Thread/ { print s; s = ""; }
+/^\#/ { if ($3 == "in") { f = $4"@"$NF } else {f = $2"@"$NF } if (s != "" ) { s = f ";" s} else { s = f } }
+END { print s }' | \
+    sort | uniq -c | sort -r -n -k 1,1 | awk '{printf("%s\t%s\n",$2,$1)}' > $output
